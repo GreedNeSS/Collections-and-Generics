b50e4cb339bc35e741b31041653e8c2514779614
GreedNeSS <ruslan199126@gmail.com>
2022-01-30 22:35:59 +0300

    Добавьте файлы проекта.

---
+++ Generic Collection/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generic_Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseGenericList();
+            Console.WriteLine();
+
+            UseGenericStack();
+            Console.WriteLine();
+
+            UseGenericQueue();
+            Console.WriteLine();
+        }
+
+        private static void UseGenericList()
+        {
+            List<Person> people = new List<Person>
+            {
+                new Person("Homer", "Simpson", 47),
+                new Person("Marge", "Simpson", 45),
+                new Person("Lisa", "Simpson", 9),
+                new Person("Bart", "Simpson", 8),
+            };
+
+            Console.WriteLine($"Items in List: {people.Count}");
+
+            foreach (Person p in people)
+            {
+                Console.WriteLine(p);
+            }
+
+            Console.WriteLine("\n->Inserting new Person: " +
+                "new Person(\"Maggie\", \"Simpson\", 2))");
+            people.Insert(2, new Person("Maggie", "Simpson", 2));
+            Console.WriteLine($"Items in List: {people.Count}");
+
+            people.ForEach(p =>
+            {
+                Console.WriteLine(p);
+            });
+
+            Person[] arrayOfPeople = people.ToArray();
+            Console.WriteLine("\npeople.ToArray()");
+
+            foreach (var item in arrayOfPeople)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void UseGenericStack()
+        {
+            Stack<Person> stackOfPeople = new Stack<Person>();
+
+            stackOfPeople.Push(new Person("Homer", "Simpson", 47));
+            stackOfPeople.Push(new Person("Marge", "Simpson", 45));
+            stackOfPeople.Push(new Person("Lisa", "Simpson", 9));
+
+            Console.WriteLine("\nStack iteration");
+
+            foreach (var item in stackOfPeople)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine($"Count people: {stackOfPeople.Count}");
+            Console.WriteLine($"Fist person is: {stackOfPeople.Peek()}");
+            Console.WriteLine($"Popped of {stackOfPeople.Pop()}");
+            Console.WriteLine($"Fist person is: {stackOfPeople.Peek()}");
+            Console.WriteLine($"Popped of {stackOfPeople.Pop()}");
+            Console.WriteLine($"Fist person is: {stackOfPeople.Peek()}");
+            Console.WriteLine($"Popped of {stackOfPeople.Pop()}");
+
+            Console.WriteLine($"\nstackOfPeople.Count: {stackOfPeople.Count}");
+            try
+            {
+                Console.WriteLine($"Fist person is: {stackOfPeople.Peek()}");
+                Console.WriteLine($"Popped of {stackOfPeople.Pop()}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"\n->Error: {e.Message}");
+            }
+        }
+
+        private static void UseGenericQueue()
+        {
+            Queue<Person> queue = new Queue<Person>();
+            queue.Enqueue(new Person("Homer", "Simpson", 47));
+            queue.Enqueue(new Person("Marge", "Simpson", 45));
+            queue.Enqueue(new Person("Lisa", "Simpson", 9));
+
+            Console.WriteLine($"\n-> queue.Count: {queue.Count}");
+            Console.WriteLine($"queue.Peek(): {queue.Peek()}");
+            Console.WriteLine($"queue.Dequeue(): {queue.Dequeue()}");
+            Console.WriteLine($"queue.Peek(): {queue.Peek()}");
+            Console.WriteLine($"queue.Dequeue(): {queue.Dequeue()}");
+            Console.WriteLine($"queue.Peek(): {queue.Peek()}");
+            Console.WriteLine($"queue.Dequeue(): {queue.Dequeue()}");
+
+            Console.WriteLine("queue.TryPeek(): {0}", 
+                queue.TryPeek(out Person person) 
+                ? person : "the queue is over");
+            Console.WriteLine("queue.TryDequeue(): {0}",
+                queue.TryDequeue(out Person person1) 
+                ? person1 : "the queue is over");
+        }
+    }
+}
---
+++ Generic Collection/SortPeopleByAge.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Collection
+{
+    class SortPeopleByAge : IComparer<Person>
+    {
+        public int Compare(Person firstPerson, Person lastPerson)
+        {
+            if (firstPerson?.Age > lastPerson?.Age)
+            {
+                return 1;
+            }
+            else if (firstPerson?.Age < lastPerson?.Age)
+            {
+                return -1;
+            }
+            return 0;
+        }
+    }
+}
