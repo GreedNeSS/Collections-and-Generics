08e293b2b8708080b462db6a29c23811564a28b3
GreedNeSS <ruslan199126@gmail.com>
2022-01-31 19:20:17 +0300

    Added more exapmles

--- Generic Collection/Program.cs
+++ Generic Collection/Program.cs
@@ -15,6 +15,12 @@ namespace Generic_Collection
 
             UseGenericQueue();
             Console.WriteLine();
+
+            UseSortedSet();
+            Console.WriteLine();
+
+            UseDictionary();
+            Console.WriteLine();
         }
 
         private static void UseGenericList()
@@ -109,6 +115,72 @@ namespace Generic_Collection
             Console.WriteLine("queue.TryDequeue(): {0}",
                 queue.TryDequeue(out Person person1) 
                 ? person1 : "the queue is over");
+        }
+
+        private static void UseSortedSet()
+        {
+            SortedSet<Person> setOfPerson = new SortedSet<Person>(new SortPeopleByAge())
+            {
+                new Person("Homer", "Simpson", 47),
+                new Person("Marge", "Simpson", 45),
+                new Person("Lisa", "Simpson", 9),
+                new Person("Bart", "Simpson", 8),
+            };
+
+            Console.WriteLine("\n-> SortedSet:");
+
+            foreach (var person in setOfPerson)
+            {
+                Console.WriteLine(person);
+            }
+
+            setOfPerson.Add(new Person("Saku", "Jones", 1));
+            setOfPerson.Add(new Person("Mikko", "Jones", 32));
+            setOfPerson.Add(new Person("Mikko", "Jones", 2));
+            setOfPerson.Add(new Person("Ruru", "Huea", 32));
+
+            Console.WriteLine();
+
+            foreach (var person in setOfPerson)
+            {
+                Console.WriteLine(person);
+            }
+        }
+
+        private static void UseDictionary()
+        {
+            Console.WriteLine("\n-> Dictionary:");
+
+            Dictionary<string, Person> peopleA = new Dictionary<string, Person>();
+            peopleA.Add("Homer", new Person("Homer", "Simpson", 47));
+            peopleA.Add("Marge", new Person("Marge", "Simpson", 45));
+            peopleA.Add("Lisa", new Person("Lisa", "Simpson", 9));
+            peopleA.Add("Bart", new Person("Bart", "Simpson", 8));
+
+            Person homer = peopleA["Homer"];
+            Console.WriteLine(homer);
+
+            Dictionary<string, Person> peopleB = new Dictionary<string, Person>
+            {
+                { "Homer", new Person("Homer", "Simpson", 47) },
+                { "Marge", new Person("Marge", "Simpson", 45) },
+                { "Lisa", new Person("Lisa", "Simpson", 9) },
+                { "Bart", new Person("Bart", "Simpson", 8) },
+            };
+
+            Person marge = peopleB["Marge"];
+            Console.WriteLine(marge);
+
+            Dictionary<string, Person> peopleC = new Dictionary<string, Person>
+            {
+                ["Homer"] = new Person("Homer", "Simpson", 47),
+                ["Marge"] = new Person("Marge", "Simpson", 45),
+                ["Lisa"] = new Person("Lisa", "Simpson", 9),
+                ["Bart"] = new Person("Bart", "Simpson", 8),
+            };
+
+            Person bart = peopleC["Bart"];
+            Console.WriteLine(bart);
         }
     }
 }
