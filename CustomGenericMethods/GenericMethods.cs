ca71341cdd1a428afca36db7e90b5388da866a61
GreedNeSS <ruslan199126@gmail.com>
2022-01-31 20:28:17 +0300

    Added generic methods

---
+++ CustomGenericMethods/GenericMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomGenericMethods
+{
+    public static class GenericMethods
+    {
+        public static void Swap<T>(ref T arg1, ref T arg2)
+        {
+            T temp = arg1;
+            arg1 = arg2;
+            arg2 = temp;
+        }
+
+        public static void DisplayBaseClass<T>()
+        {
+            Console.WriteLine($"Base class of {typeof(T)} is: {typeof(T).BaseType}");
+        }
+    }
+}
---
+++ CustomGenericMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CustomGenericMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Henry", "Ford", 45);
+            Person p2 = new Person("Homer", "Simonov", 49);
+
+            Console.WriteLine($"Before Swap() p1: {p1}, p2: {p2}");
+            GenericMethods.Swap<Person>(ref p1, ref p2);
+            Console.WriteLine($"After Swap() p1: {p1}, p2: {p2}");
+
+            string str1 = "Hello";
+            string str2 = "World";
+
+            Console.WriteLine($"Before Swap() str1: {str1}, str2: {str2}");
+            GenericMethods.Swap<string>(ref str1, ref str2);
+            Console.WriteLine($"After Swap() str1: {str1}, str2: {str2}");
+
+            GenericMethods.DisplayBaseClass<Person>();
+            GenericMethods.DisplayBaseClass<int>();
+            GenericMethods.DisplayBaseClass<string>();
+        }
+    }
+}
