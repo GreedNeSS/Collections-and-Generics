add3051f969cb3e8f59dbf5295c018e8842be1cc
GreedNeSS <ruslan199126@gmail.com>
2022-01-31 20:49:06 +0300

    Added Generic Struct

---
+++ Generic struct/Point.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_struct
+{
+    struct Point<T>
+    {
+        private T xPos;
+        private T yPos;
+
+        public T X
+        {
+            get { return xPos; }
+            set { xPos = value; }
+        }
+
+        public T Y
+        {
+            get { return yPos; }
+            set { yPos = value; }
+        }
+
+        public Point(T xVal, T yVal)
+        {
+            xPos = xVal;
+            yPos = yVal;
+        }
+
+        public override string ToString() => $"[{X}, {Y}]";
+
+        public void ResetPoint()
+        {
+            X = default(T);
+            Y = default(T);
+        }
+    }
+}
---
+++ Generic struct/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Generic_struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point<int> point1 = new Point<int>(10, 10);
+
+            Console.WriteLine($"point1.ToString(): {point1.ToString()}");
+            point1.ResetPoint();
+            Console.WriteLine($"point1.ToString(): {point1.ToString()}");
+            
+            Point<decimal> point2 = new Point<decimal>(0.434m, 10.54m);
+
+            Console.WriteLine($"point2.ToString(): {point2.ToString()}");
+            point2.ResetPoint();
+            Console.WriteLine($"point2.ToString(): {point2.ToString()}");
+        }
+    }
+}
